488f47a73afea1e3a40dbf2ca5c648b121244179
Gev2209 <mirzoyan.gevorg.23@gmail.com>
2024-02-13 01:53:30 +0400

    Starting web page. Good Luck

---
+++ zexample/index.js
@@ -0,0 +1,79 @@
+// const arr = [1,2,3,4,5];
+
+
+// let sum = 0;
+
+// for (let i = 0; i < 3; i--) {
+//     console.log(i)
+// }
+
+
+
+
+
+
+
+
+// for (var i = 0; i < 10; i++) {
+//     sum = sum + i
+// }
+
+// console.log(i)
+// console.log(sum)
+
+
+// let count = 0;
+// console.log(count++);
+// console.log(count++);
+// console.log(++count);
+
+
+
+// class Car {
+//     constructor (name,color,age) {
+//         this.name = name;
+//         this.color = color;
+//         this.age = age
+//     }
+
+//     volume (voice) {
+//         alert(voice)
+//     }
+
+//     get dog () {
+//         alert('Gevorg')       
+//     }
+// }
+
+// const newCar = new Car('shark','gray',6);
+// newCar.volume
+
+// console.log(newCar)
+
+
+// let str = 'hello';
+// let str2 = str.toLocaleUpperCase()
+// let num = 85
+
+// let float = 54651.589
+// console.log(str2)
+// console.log(num.toString(36))
+// console.log(float.toPrecision(2))
+
+// console.log('hello' + {});
+
+// let num = [3,0,6]
+// let sum = 0;
+// for (let i = 0; i < num.length; i++) {
+//    sum = sum + num[i] 
+// }
+// console.log(sum)
+
+
+
+
+// const line = document.querySelector('span');
+
+// line.addEventListener('mouseenter', function () {
+//     line.style.border = '3px solid red'
+// })
