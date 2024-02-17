795ba615d495cf318ff8fe009c23227c50494d6a
Gev2209 <mirzoyan.gevorg.23@gmail.com>
2024-02-17 17:47:50 +0400

    adding code in arm,rus

--- enSide/en.js
+++ enSide/en.js
@@ -1,20 +1,4 @@
-import { line,line1,line2,line3,home,about,contact,portfolio,code,info,arm,rus,en,login,log } from "./helpers"
-// const line = document.querySelector('.infoLine');
-// const line1 = document.querySelector('.infoLine1');
-// const line2 = document.querySelector('.infoLine2');
-// const line3 = document.querySelector('.infoLine3');
-// const home = document.querySelector('.home');
-// const about = document.querySelector('.about');
-// const contact = document.querySelector('.contact');
-// const portfolio = document.querySelector('.portfolio');
-// const code = document.querySelector('.fa-code');
-// const info = document.querySelector('.info');
-// const arm = document.querySelector('.arm');
-// const rus = document.querySelector('.rus');
-// const en = document.querySelector('.en');
-// const login = document.querySelector('.login_box');
-// const log = document.querySelector('.header_login');
-
+import { line,line1,line2,line3,home,about,contact,portfolio,code,info,arm,rus,en,login,log } from "./helpers.js"
 
 // const loginHeader = () => {
 //     login.style.transform = 'translate(0)';
