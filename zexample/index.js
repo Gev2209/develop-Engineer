b3898145c1056159f2c5339920bdc17f8613796e
Gev2209 <mirzoyan.gevorg.23@gmail.com>
2024-02-19 21:14:09 +0400

    create languages group with languages groups and modified lesson section all in a tag

--- zexample/index.js
+++ zexample/index.js
@@ -77,3 +77,37 @@
 // line.addEventListener('mouseenter', function () {
 //     line.style.border = '3px solid red'
 // })
+
+
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+fetch(url)
+.then(data => {
+    console.log(data);
+    // data.text().then(data2 => {
+    //     console.log(data2)
+    // })
+    return data.text()
+})
+.then(data => {
+    console.log(data)
+})
+
+fetch(url, {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *client
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  })
+
+  .then(response => response.text())
+  .then(response => {
+    console.log(response)
+  })
